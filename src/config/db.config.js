12443049a3a327e93e360421ffdd9ba739d2e6f0
illakoroliuk <illakoroluk62@gmail.com>
2025-07-05 15:38:32 +0300

    added db.config.js

---
+++ src/config/db.config.js
@@ -0,0 +1,22 @@
+const knex = require('knex');
+var dbConfig = require("./knexfile");
+
+const db = knex(dbConfig.development);
+
+db.raw('SELECT 1')
+  .then(() => console.log('Database connected'))
+  .catch((err) => {
+    console.error('Database connection failed:', err);
+    process.exit(1);
+  });
+
+const dbHealthMiddleware = (req, res, next) => {
+  db.raw('SELECT 1')
+    .then(() => next())
+    .catch(() => res.status(503).json({ error: 'Database unavailable' }));
+};
+
+module.exports = {
+    db,
+    dbHealthMiddleware
+};
